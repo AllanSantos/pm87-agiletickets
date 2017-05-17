8073b1ba0b66a599156cf5d78b3ac87e1127e16c
Allan <allan.jre@gmail.com>
2017-05-16 22:38:49 -0300

    Deletado If de orquestra e reaproveitando o codigo, no If do Ballet, pois usa a mesma regra de negocio utilizando o operador ||

--- src/main/java/br/com/caelum/agiletickets/domain/precos/CalculadoraDePrecos.java
+++ src/main/java/br/com/caelum/agiletickets/domain/precos/CalculadoraDePrecos.java
@@ -9,49 +9,33 @@ public class CalculadoraDePrecos {
 
 	public static BigDecimal calcula(Sessao sessao, Integer quantidade) {
 		BigDecimal preco;
-		
-		
-		
-		if(sessao.getEspetaculo().getTipo().equals(TipoDeEspetaculo.CINEMA) || sessao.getEspetaculo().getTipo().equals(TipoDeEspetaculo.SHOW)) {
-			//quando estiver acabando os ingressos... 
-			if(calculaPercentualDeLotacao(sessao) <= 0.05) { 
+
+		if (sessao.getEspetaculo().getTipo().equals(TipoDeEspetaculo.CINEMA)
+				|| sessao.getEspetaculo().getTipo().equals(TipoDeEspetaculo.SHOW)) {
+			
+			if (calculaPercentualDeLotacao(sessao) <= 0.05) {
 				preco = sessao.getPreco().add(sessao.getPreco().multiply(BigDecimal.valueOf(0.10)));
 			} else {
 				preco = sessao.getPreco();
 			}
-			
-			
-			
-		} else if(sessao.getEspetaculo().getTipo().equals(TipoDeEspetaculo.BALLET)) {
-			if(calculaPercentualDeLotacao(sessao) <= 0.50) { 
-				preco = sessao.getPreco().add(sessao.getPreco().multiply(BigDecimal.valueOf(0.20)));
-			} else {
-				preco = sessao.getPreco();
-			}
-			
-			if(sessao.getDuracaoEmMinutos() > 60){
-				preco = preco.add(sessao.getPreco().multiply(BigDecimal.valueOf(0.10)));
-			}
-			
-			
-			
-		} else if(sessao.getEspetaculo().getTipo().equals(TipoDeEspetaculo.ORQUESTRA)) {
-			if(calculaPercentualDeLotacao(sessao) <= 0.50) { 
+
+		} else if (sessao.getEspetaculo().getTipo().equals(TipoDeEspetaculo.BALLET)
+				|| sessao.getEspetaculo().getTipo().equals(TipoDeEspetaculo.ORQUESTRA)) {
+			if (calculaPercentualDeLotacao(sessao) <= 0.50) {
 				preco = sessao.getPreco().add(sessao.getPreco().multiply(BigDecimal.valueOf(0.20)));
 			} else {
 				preco = sessao.getPreco();
 			}
 
-			if(sessao.getDuracaoEmMinutos() > 60){
+			if (sessao.getDuracaoEmMinutos() > 60) {
 				preco = preco.add(sessao.getPreco().multiply(BigDecimal.valueOf(0.10)));
 			}
-		}  
-		
-		
-		
+
+		}
+
 		else {
 			preco = sessao.getPreco();
-		} 
+		}
 
 		return preco.multiply(BigDecimal.valueOf(quantidade));
 	}
